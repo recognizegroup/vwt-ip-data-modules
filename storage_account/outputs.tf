4bda4350971458ddbd32c73dba6ec73cbe9aed25
Jeffrey Verpoort <j.verpoort@vwtelecom.com>
2022-10-03 14:13:38 +0200

    Add missing but required output variables

--- storage_account/outputs.tf
+++ storage_account/outputs.tf
@@ -5,3 +5,8 @@ output "id" {
 output "name" {
   value = data.azurerm_storage_account.example.name
 }
+
+output "primary_access_key" {
+  value     = data.azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
