100cd11bb9057efc481079fa94f711b60eb1fb1d
Jeffrey Verpoort <j.verpoort@vwtelecom.com>
2022-10-03 14:20:44 +0200

    More fixes to module

--- storage_account/main.tf
+++ storage_account/main.tf
@@ -14,7 +14,7 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_storage_account" "example" {
+data "azurerm_storage_account" "storage_account" {
   name                = var.name
   resource_group_name = var.resource_group_name
 }
