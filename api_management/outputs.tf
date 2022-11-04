d5860d12320c2873dddc7cba1f9ee594149e5b52
Justin-Booij <j.booij@recognize.nl>
2022-11-04 15:32:31 +0100

    add principal id output

--- api_management/outputs.tf
+++ api_management/outputs.tf
@@ -1,3 +1,7 @@
 output "developer_portal_url" {
   value = data.azurerm_api_management.api_management.developer_portal_url
 }
+
+output "principal_id" {
+  value = data.azurerm_api_management.api_management.identity.principal_id
+}
