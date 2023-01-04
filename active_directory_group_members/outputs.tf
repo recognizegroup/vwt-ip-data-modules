aa11a1f0aee4ef4544c3a7bf5c1ee25e59071f73
corjanZee <c.e.zee@recognize.nl>
2023-01-04 15:07:52 +0100

    Active directory group members workitem 11141

---
+++ active_directory_group_members/outputs.tf
@@ -0,0 +1,7 @@
+﻿output "members" {
+  value = data.azuread_group.active_directory_group.members
+}
+
+output "owners" {
+  value = data.azuread_group.active_directory_group.owners
+}
---
+++ active_directory_group_members/variables.tf
@@ -0,0 +1,4 @@
+﻿variable "active_directory_group_display_name" {
+  description = "Display name of the active directory group"
+  type = string
+}
