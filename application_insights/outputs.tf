49b4f20c8eb4842045ae1b96f5ef73febba3beb8
corjanZee <c.e.zee@recognize.nl>
2023-01-24 14:22:48 +0100

    Application Id data module #ab12866 task 12918

--- application_insights/outputs.tf
+++ application_insights/outputs.tf
@@ -2,3 +2,10 @@ output "instrumentation_key" {
   value = data.azurerm_application_insights.application_insights.instrumentation_key
 }
 
+output "id" {
+  value = data.azurerm_application_insights.application_insights.id
+}
+
+output "app_id" {
+  value = data.azurerm_application_insights.application_insights.app_id
+}
