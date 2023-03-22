284c3b5f7b132d8095ff1b59238ff7c8fe664815
corjanZee <c.e.zee@recognize.nl>
2023-03-22 07:40:59 +0100

    Service plan module will replace app service plan AB#14277

---
+++ service_plan/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group name the asp resource is part of"
+}
+
+variable "service_plan_name" {
+  type        = string
+  description = "The name of the asp resource"
+}
