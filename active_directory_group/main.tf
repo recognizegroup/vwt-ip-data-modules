cfbe738987ce71fa6657c8f005c4a9dd77da6a99
corjanZee <c.e.zee@recognize.nl>
2023-05-01 15:09:45 +0200

    Search active directories by object id AB#15055

--- active_directory_group/main.tf
+++ active_directory_group/main.tf
@@ -13,5 +13,6 @@
 
 data "azuread_group" "active_directory_group" {
   display_name = var.active_directory_group_display_name
+  object_id = var.active_directory_object_id
   security_enabled = true
 }
