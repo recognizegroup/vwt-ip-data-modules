af8e8b58ed441e89f847e499d2a2452b1c5933b1
Michal Pipal <m.pipal@recognize.nl>
2023-10-15 22:39:02 +0200

    Added data module for virtual network

---
+++ virtual_network/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type        = string
+  description = "The name of the virtual network."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which the vnet."
+}
