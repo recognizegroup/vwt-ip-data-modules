b49826b906251f139afd11b581df63cb90d19b89
Michal Pipal <m.pipal@recognize.nl>
2023-10-16 08:47:56 +0200

    Update comment

--- virtual_network/variables.tf
+++ virtual_network/variables.tf
@@ -5,5 +5,5 @@ variable "name" {
 
 variable "resource_group_name" {
   type        = string
-  description = "The name of the resource group in which the vnet."
+  description = "The name of the resource group in which the vnet is located."
 }
