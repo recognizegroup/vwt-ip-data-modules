2f33ef0dbd2572732d2b1d08a65c7bf6c2efb87e
Marian Kostal <m.kostal@recognize.nl>
2023-11-28 13:59:13 +0100

    Added Events Hub data module

---
+++ eventhub_namespace/variables.tf
@@ -0,0 +1,9 @@
+variable "event_hub_namespace" {
+  type        = string
+  description = "The event hub namespace to retrieve"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group the event hub namespace is in"
+}
