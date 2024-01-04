bc117cc65a8f168f077e5491b9a8cc3abb856c88
Michal Pipal <m.pipal@recognize.nl>
2024-01-04 13:51:58 +0100

    Extend service_bus_namespace data module output AB#18332

--- servicebus_namespace/outputs.tf
+++ servicebus_namespace/outputs.tf
@@ -1,3 +1,15 @@
 output "id" {
   value = data.azurerm_servicebus_namespace.service_bus_namespace.id
 }
+
+output "https_endpoint" {
+  value = data.azurerm_servicebus_namespace.service_bus_namespace.endpoint
+}
+
+output "sb_endpoint" {
+  value = "sb://${data.azurerm_servicebus_namespace.service_bus_namespace.name}.servicebus.windows.net"
+}
+
+output "name" {
+  value = data.azurerm_servicebus_namespace.service_bus_namespace.name
+}
