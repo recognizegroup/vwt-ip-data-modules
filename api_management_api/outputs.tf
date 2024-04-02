8b9b6608af75c71faf97bda083b47813b583c54f
ArtiomMatiom <89966532+ArtiomMatiom@users.noreply.github.com>
2024-04-02 15:09:45 +0200

    Add api_management_api data module (#28)

---
+++ api_management_api/outputs.tf
@@ -0,0 +1,15 @@
+output "resource_group_name" {
+  value = var.resource_group_name
+}
+
+output "api_management_name" {
+  value = var.api_management_name
+}
+
+output "id" {
+  value =  data.azurerm_api_management_api.api.id
+}
+
+output "client_id" {
+  value = var.app_registration_name != null ? data.azuread_application.app_registration[0].client_id : null
+}
