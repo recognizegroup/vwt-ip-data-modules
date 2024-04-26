9b506a3662ae414f452034658f945632d524ab18
Michal Pipal <m.pipal@recognize.nl>
2024-04-26 13:52:31 +0200

    Workaround for azurerm issue with inconsistent ServicePlan ID

--- service_plan/outputs.tf
+++ service_plan/outputs.tf
@@ -1,3 +1,10 @@
 output "id" {
   value = data.azurerm_service_plan.service_plan.id
 }
+
+// Fixing the provider bugs, which provides resource ID in the format which is not accepted by Func Apps 
+// According to the azurerm team it was fixed multiple times, but we still observe it
+// https://github.com/hashicorp/terraform-provider-azurerm/issues/24560
+output "camel_case_id" {
+  value = replace(data.azurerm_service_plan.service_plan.id, "serverfarms", "serverFarms")
+}
