2c11d7655d4d350922e105366c5e086ac39ac17b
Michal Pipal <73311540+pipalmic@users.noreply.github.com>
2025-03-31 14:55:08 +0200

    Merge pull request #32 from recognizegroup/feature/output-apim-id
    feat: Added APIM ID to data module output AB#00000

--- api_management/outputs.tf
+++ api_management/outputs.tf
@@ -1,3 +1,7 @@
+output "id" {
+  value = data.azurerm_api_management.api_management.id
+}
+
 output "developer_portal_url" {
   value = data.azurerm_api_management.api_management.developer_portal_url
 }
