7908c93f2b966d2481f593019ffcd980ceb17535
Kelly Faessen <kelly@faessen.com>
2026-09-01 10:47:45 +0200

    chore: drop the workspace shared key output AB#9316

--- loganalytics_workspace/outputs.tf
+++ loganalytics_workspace/outputs.tf
@@ -1,8 +1,3 @@
-output "primary_key" {
-  value     = data.azurerm_log_analytics_workspace.log_analytics_workspace.primary_shared_key
-  sensitive = true
-}
-
 output "workspace_id" {
   value = data.azurerm_log_analytics_workspace.log_analytics_workspace.workspace_id
 }
